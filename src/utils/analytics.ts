d6efa0a032e6d6f18af245e9cc81b09eb714b3a0
Jacky Cheung <cheungkwingho@gmail.com>
2023-08-20 21:34:31 +0800

    upgrade to ga4

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,9 +1,9 @@
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 
 export function initalizeAnalytics() {
   ReactGA.initialize(import.meta.env.VITE_GA_ID);
 }
 
 export function trackPageView(page: string) {
-  ReactGA.pageview(page);
+  ReactGA.send({ hitType: "pageview", page });
 }
