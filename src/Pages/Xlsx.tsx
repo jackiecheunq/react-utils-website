7b625ebfc8574857804b69dd2642c66d44fc0134
Jacky Cheung <cheungkwingho@gmail.com>
2023-12-07 00:42:07 +0800

    feature: xlsx add split mode and number filter

--- src/Pages/Xlsx.tsx
+++ src/Pages/Xlsx.tsx
@@ -6,33 +6,85 @@ import { toast } from "react-toastify";
 const Xlsx = () => {
   const [title, setTitle] = useState("id");
   const [content, setContent] = useState("");
+  const [numberLengthForFiltering, setNumberLengthForFiltering] =
+    useState<number>(0);
+  const [isSplitModeEnable, setIsSplitModeEnable] = useState(true);
+  const separator = "@";
 
-  const convert = (src: string) => {
-    const ids = src.match(/\b\d{6,}\b/g);
-    let result = ids
-      ?.filter((id) => {
-        return id.length === 6 || id.length === 9;
-      })
-      .map((id) => {
-        return id.length === 6 ? id + "000" : id;
-      });
+  const convert = (
+    src: string,
+    filterFn: (id: string) => boolean,
+    isSplitModeEnable: boolean
+  ) => {
+    let result = [];
+    const regex = /\b\d{6,}\b/g;
+    const filterAndAdjust = (ids: RegExpMatchArray | null) => {
+      return ids
+        ?.filter((id) => {
+          return filterFn(id);
+        })
+        .map((id) => {
+          return id.length === 6 ? id + "000" : id;
+        });
+    };
+    if (isSplitModeEnable) {
+      const splitedString = src.split(separator);
+      for (let index = 0; index < splitedString.length; index++) {
+        const ids = splitedString[index].match(regex);
+        const res = filterAndAdjust(ids);
+        if (res) {
+          result.push([...new Set(res)]);
+        }
+      }
+    } else {
+      const ids = src.match(regex);
+      const res = filterAndAdjust(ids);
+      return res ? [...new Set(res)] : res;
+    }
 
-    return result ? [...new Set(result)] : result;
+    return result;
   };
 
   const convertedValue = useMemo(() => {
-    return convert(content);
-  }, [content]);
+    const numberLengthfilter = (id: string) => {
+      if (numberLengthForFiltering > 0) {
+        return id.length === numberLengthForFiltering;
+      } else {
+        return id.length === 6 || id.length === 9;
+      }
+    };
+    return convert(content, numberLengthfilter, isSplitModeEnable);
+  }, [content, numberLengthForFiltering, isSplitModeEnable]);
 
   const ouputHandler = () => {
-    const src = convertedValue;
-    if (src) {
-      const workBook = utils.book_new();
-      const workSheet = utils.aoa_to_sheet([[], ...src.map((id) => [id])]);
-      utils.book_append_sheet(workBook, workSheet, title || "id");
-      writeFileXLSX(workBook, (title || "id") + ".xlsx");
+    try {
+      const src = convertedValue;
+      const xlsxOupput = (src: string[]) => {
+        const workBook = utils.book_new();
+        const workSheet = utils.aoa_to_sheet([[], ...src.map((id) => [id])]);
+        utils.book_append_sheet(workBook, workSheet, title || "id");
+        writeFileXLSX(workBook, (title || "id") + ".xlsx");
+      };
+      if (Array.isArray(src)) {
+        if (isSplitModeEnable) {
+          src.forEach((stringArr) => {
+            if (Array.isArray(stringArr)) {
+              xlsxOupput(stringArr);
+            }
+          });
+        } else {
+          if (src.length > 0 && src.every((i) => typeof i === "string")) {
+            xlsxOupput(src as string[]);
+          }
+        }
+      }
+
       setContent("");
       toast.success("Success!");
+    } catch (error) {
+      if (error instanceof Error) {
+        toast.error(error.message);
+      }
     }
   };
 
@@ -57,6 +109,77 @@ const Xlsx = () => {
           }}
           value={content}
         />
+        <div className="flex [&>*]:mr-6">
+          <div
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setIsSplitModeEnable(event.target.checked);
+            }}
+            className="border p-3 border-black"
+          >
+            <legend>
+              Enable <span className="font-bold">Split</span> Mode: <br />
+              (Separator: @)
+            </legend>
+
+            <div>
+              <input
+                type="checkbox"
+                id="splitModeEnableCheckbox"
+                name="splitModeEnableCheckbox"
+                checked={isSplitModeEnable}
+                readOnly
+              />
+              <label htmlFor="splitModeEnableCheckbox"> Enable</label>
+            </div>
+          </div>
+        </div>
+        <div className="flex [&>*]:mr-6">
+          <div
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setNumberLengthForFiltering(+event.target.value);
+            }}
+            className="border p-3 border-black"
+          >
+            <legend>
+              Select a <span className="font-bold">filter</span> Mode:{" "}
+            </legend>
+
+            <div>
+              <input
+                type="radio"
+                id="default"
+                name="numberLengthForFiltering"
+                value={0}
+                checked={numberLengthForFiltering === 0}
+                readOnly
+              />
+              <label htmlFor="default"> Default(6 or 9)</label>
+            </div>
+
+            <div>
+              <input
+                type="radio"
+                id="six"
+                name="numberLengthForFiltering"
+                value={6}
+                checked={numberLengthForFiltering === 6}
+                readOnly
+              />
+              <label htmlFor="six"> Six(6)</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                id="six"
+                name="numberLengthForFiltering"
+                value={9}
+                checked={numberLengthForFiltering === 9}
+                readOnly
+              />
+              <label htmlFor="six"> Nine(9)</label>
+            </div>
+          </div>
+        </div>
         <button
           className="btn py-4 px-12 block disabled:cursor-not-allowed disabled:opacity-75"
           onClick={ouputHandler}
@@ -70,10 +193,18 @@ const Xlsx = () => {
         <h3 className="text-3xl ">
           Total Number:{" "}
           <span className="font-bold text-red-600">
-            {convertedValue?.length || 0}
+            {convertedValue?.flat().length || 0}
           </span>
         </h3>
-        <p className="min-w-[80%]">{convertedValue?.join(", ") || ""}</p>
+        <div className="min-w-[80%]">
+          {isSplitModeEnable
+            ? convertedValue?.map((value, index) => (
+                <p key={`group${index}`}>
+                  Group {index}: {Array.isArray(value) ? value?.join(", ") : ""}
+                </p>
+              ))
+            : convertedValue?.join(", ") || ""}
+        </div>
       </div>
     </div>
   );
